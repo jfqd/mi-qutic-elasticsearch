4cbe7eb6c8806056df985ea128051746ede5382a
jfqd <jfqd@blun.org>
2019-01-04 09:20:30 +0100

    add httpasswd support to nginx

--- copy/var/zoneinit/includes/46-nginx.sh
+++ copy/var/zoneinit/includes/46-nginx.sh
@@ -6,5 +6,15 @@ NGINX_HOME='/opt/local/etc/nginx'
 gsed -i "s:__HOSTNAME__:${HOST}:g" \
 	${NGINX_HOME}/nginx.conf
 
+# create htpasswd file
+if mdata-get nginx_htpasswd 1>/dev/null 2>&1; then
+  user=$(mdata-get nginx_htpasswd | tr ":"  "\n" | sed -n 1p)
+  pass=$(mdata-get nginx_htpasswd | tr ":"  "\n" | sed -n 2p)
+  crypt=$(openssl passwd -apr1 $pass)
+  echo "$user:$crypt" > /opt/local/etc/nginx/.htpasswd
+  chown www:root /opt/local/etc/nginx/.htpasswd
+  chmod 0640 /opt/local/etc/nginx/.htpasswd
+fi
+
 # Enable nginx
 svcadm enable svc:/pkgsrc/nginx:default
